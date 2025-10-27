df70b4b63b3123cbe6973403074d757c2531aa06
Jan Lipponen <jan.lipponen@sorvimo.com>
2025-10-27 16:03:29 +0000

    Add dummy edit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ export default function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
       <h1>PatinaWilla</h1>
-      <p>Hello World 👋</p>
+      <p>Hello World 👋 This is a dummy edit</p>
     </div>
   );
 }
